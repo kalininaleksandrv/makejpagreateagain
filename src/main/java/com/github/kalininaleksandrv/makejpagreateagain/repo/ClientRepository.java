c77754cd1b62045064923a76528a90a1a88cbe62
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2023-03-12 13:27:43 +0100

    fix open-in vew and lazy init on client

--- src/main/java/com/github/kalininaleksandrv/makejpagreateagain/repo/ClientRepository.java
+++ src/main/java/com/github/kalininaleksandrv/makejpagreateagain/repo/ClientRepository.java
@@ -4,6 +4,8 @@ import com.github.kalininaleksandrv.makejpagreateagain.model.Client;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends CrudRepository<Client, Integer> {
     Client findByName(String name);
 
@@ -11,4 +13,9 @@ public interface ClientRepository extends CrudRepository<Client, Integer> {
             value = Client.CLIENT_ACCOUNT_CONTACT_ENTITY_GRAPH)
     @Override
     Iterable<Client> findAll();
+
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH,
+            value = Client.CLIENT_ACCOUNT_CONTACT_ENTITY_GRAPH)
+    @Override
+    Optional<Client> findById(Integer id);
 }
